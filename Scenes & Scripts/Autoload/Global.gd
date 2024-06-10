67dde3a7cc2d807abfcbf94c00d72f7b6ff33359
sebashtioon <sebastiansuciu607@gmail.com>
2024-06-11 07:32:12 +1000

    OC6

--- Scenes & Scripts/Autoload/Global.gd
+++ Scenes & Scripts/Autoload/Global.gd
@@ -1,27 +1,43 @@
 extends Node
 
+# Global script :)
+
+var save_path_local = "user://variable.save"
 
 
-# global script :)
 
 var save_data = {
 	
+	# Auth (Not in use right now)
 	"USERNAME": null,
 	"PASSWORD": null,
 	
 	
 	
+	# Values
 	"COINS":0,
 	"COINSPERSECOND":0,
 	"PLASMA":0,
-	
-	
-	
 }
+
+func save_data_local():
+	var file = FileAccess.open(save_path_local, FileAccess.WRITE)
+	file.store_var(save_data)
+
+func load_data_local():
+	if FileAccess.file_exists(save_path_local):
+		var file = FileAccess.open(save_path_local, FileAccess.READ)
+		save_data = file.get_var(save_data)
+	else:
+		print("Data did not load because the path doesn't exist.")
+		print("This is probably because you opened the game for the first time.")
+		print("Creating save path...")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_data_local()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
